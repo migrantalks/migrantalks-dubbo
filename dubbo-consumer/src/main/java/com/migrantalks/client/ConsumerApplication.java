e0f6b067400e326746ba14e96eb7ac40496cfd61
zhongxia <zhongxia0807@163.com>
2019-04-19 21:12:35 +0800

    dubbo-consumer

---
+++ dubbo-consumer/src/main/java/com/migrantalks/client/ConsumerApplication.java
@@ -0,0 +1,26 @@
+package com.migrantalks.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * dubbo consumer 主程序
+ * @Author: zgs
+ * @date 2019/04/19
+ **/
+@SpringBootApplication
+@ImportResource(value = {"classpath:spring/dubbo-consumer.xml"})
+public class ConsumerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ConsumerApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
---
+++ dubbo-consumer/src/main/java/com/migrantalks/client/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.migrantalks.client.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.migrantalks.api.response.Result;
+import com.migrantalks.api.service.IUserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * dubbo client 测试
+ * @author zgs
+ * @date 2019/04/19
+ */
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    @Reference(version = "1.0")
+    IUserService userService;
+
+    @GetMapping("user")
+    public Result findUser() {
+        return userService.findUser();
+    }
+}
